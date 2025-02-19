6308432205b7a9cd5001587c5e52efce6e3302cb
Suman Tanwar <stanwar2030@gmail.com>
2025-02-19 07:12:48 -0500

    Weather check

--- lib/WeatherPage.dart
+++ lib/WeatherPage.dart
@@ -1,55 +1,76 @@
-import 'package:flutter/material.dart';
 
-class Weatherpage extends StatefulWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
+final apiKey = "f64bf0682e08dc140031412b5a1c04ff";
+
+class WeatherPage extends StatefulWidget {
+
   @override
-  State<Weatherpage> createState() => _WeatherpageState();
+  State<WeatherPage> createState() => _WeatherPageState();
 }
 
-class _WeatherpageState extends State<Weatherpage> {
-  List<String> cities = [
-    "Montreal",
-    "Toronto",
-    "Winnipeg",
-    "Saskatchewan",
-    "Delhi",
-    "Ottawa",
-    "Gurgaon",
-    "Palwal",
-    "Faridabad"
-  ];
+class _WeatherPageState extends State<WeatherPage> {
 
-  String cityName = "";
+  List<String> cities = [
+    "Montreal","Toronto","Winnipeg","Ottawa","Goa","Delhi","Palwal","Gurgaon",
+  ];
+  String weather = "";
+  String selectedCity = "";
+
+  Future<void> fetchWeather(String cityName) async {
+
+    final url = Uri.parse("https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey&units=metric");
+    final response = await get(url);
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      double temp = data["main"]["temp"];
+      int tempInt = temp.toInt();
+
+      setState(() {
+        weather = "$tempInt °C";
+        selectedCity = cityName;
+      });
+    }
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
+        title: Text("Weather Check"),
+        centerTitle: true,
         actions: [
-          PopupMenuButton(
-            onSelected: (String city) {
-              setState(() {
-                cityName = city;
-              });
-            },
-            itemBuilder: (BuildContext context) {
-              return cities.map<PopupMenuItem<String>>((String city) {
-                return PopupMenuItem<String>(
-                  value: city,
-                  child: Text(city),
-                );
-              }).toList();
-            },
-            icon: Icon(Icons.more_vert),
-          ),
+  PopupMenuButton(
+      onSelected: (String city){
+        fetchWeather(city);
+      },
+      itemBuilder: (BuildContext context) {
+        return cities.map<PopupMenuItem<String>>((String city) {
+          return PopupMenuItem(
+              value: city,
+              child: Text(city)
+          );
+        }).toList();
+      },
+    icon: Icon(Icons.more_vert),
+  
+  )
         ],
       ),
-      body: Center(
-        child: Text(
-          cityName.isEmpty ? 'Select a city' : cityName,
-          style: TextStyle(fontSize: 24),
+      body:  Center(
+        child: selectedCity.isEmpty ? Text("Select a city") : Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Weather of $selectedCity"),
+            Text(weather)
+          ],
         ),
-      ),
+      )
     );
   }
 }
